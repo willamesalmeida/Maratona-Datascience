482d708ac7625dfe8d20f69bce6df1a171c27f6c
willames5480@gmail.com <willames5480@gmail.com>
2018-12-28 17:24:56 -0300

    fase 1 da maratona

---
+++ Semana 1 - O poderoso Python - Aulas/fase 1/Python - Aula4.py
@@ -0,0 +1,15 @@
+#listas
+frase = 'Curso em video python'
+lista_nomes = ['joão', 'maria', 'willames', 'diego']
+""" for i in range(len(lista_nomes)):
+    print('PRIMEIRO USO DO SORTED')
+    print ('posição do nome é: {} e o nome é: {}'.format(i+1, sorted(lista_nomes[i])))
+
+for k in sorted(lista_nomes):
+    print('SEGUNDO USO DO SORTED')
+    print ('esse é o k  {} '.format(k))
+    print('TERCEIRO USO DO SORTED')
+    print(sorted(lista_nomes))
+ """
+print('SLICE')
+print(frase[9:21:2])
