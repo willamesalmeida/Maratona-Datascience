96294763695e35813b13fe73ecab99fee8fa6a7c
José Willames <38707851+willamesalmeida@users.noreply.github.com>
2018-12-28 18:26:21 -0200

    Delete Python - Aula2.py

--- Semana 1 - O poderoso Python - Aulas/Python - Aula2.py
+++
@@ -1,6 +0,0 @@
-#tipo de dados
-nome = input("digite seu nome: ")
-idade = input("digite sua idade: ")
-altura = input("digite sua altura: ")
-
-print("Seu nome é {} sua altura é {} e sua idade é {}".format(nome, altura, idade))
