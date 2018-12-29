0acd7d188f597d0b12a25a52a31e674abcd97f2c
José Willames <38707851+willamesalmeida@users.noreply.github.com>
2018-12-29 03:14:42 -0200

    Update Python - Aula3 - Operadores logicos e dados booleno .py

--- Semana 1 - O poderoso Python - Aulas/fase 1/Python - Aula3 - Operadores logicos e dados booleno .py
+++ Semana 1 - O poderoso Python - Aulas/fase 1/Python - Aula3 - Operadores logicos e dados booleno .py
@@ -4,3 +4,28 @@ var_falsa = False
 
 if var_verdadeiro == True:
     print("var_verdadeiro é verdade")
+if var_falsa == False:
+    print("var_falsa é falso")
+    
+#quadrante no plano cartesiano
+x, y = map(float, input().split())
+
+if x > 0 and y > 0:
+    print ('primeiro')
+elif x < 0 and y > 0:
+    print ('segundo')
+elif x < 0 and y < 0:
+    print ('terceiro')
+elif x > 0 and y < 0:
+    print ('quarto')
+elif x <0 and y ==0 :
+    print('eixo x')
+elif x >0 and y ==0 :
+    print('eixo x')
+elif x==0 and y >0 :
+    print ('eixo y')
+elif x==0 and y <0 :
+    print ('eixo y')
+elif x==0 and y ==0 :
+    print ('origem')
+
