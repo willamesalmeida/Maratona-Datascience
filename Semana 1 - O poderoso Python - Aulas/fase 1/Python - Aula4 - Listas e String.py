a5c80024c790b863c619f95fab0e145f51737980
José Willames <38707851+willamesalmeida@users.noreply.github.com>
2018-12-29 03:22:14 -0200

    Update Python - Aula4 - Listas e String.py

--- Semana 1 - O poderoso Python - Aulas/fase 1/Python - Aula4 - Listas e String.py
+++ Semana 1 - O poderoso Python - Aulas/fase 1/Python - Aula4 - Listas e String.py
@@ -1,7 +1,8 @@
-#listas
-frase = 'Curso em video python'
+#listas e String
+frase = 'Curso de python'
 lista_nomes = ['joão', 'maria', 'willames', 'diego']
-""" for i in range(len(lista_nomes)):
+
+for i in range(len(lista_nomes)):
     print('PRIMEIRO USO DO SORTED')
     print ('posição do nome é: {} e o nome é: {}'.format(i+1, sorted(lista_nomes[i])))
 
@@ -10,6 +11,6 @@ for k in sorted(lista_nomes):
     print ('esse é o k  {} '.format(k))
     print('TERCEIRO USO DO SORTED')
     print(sorted(lista_nomes))
- """
+    
 print('SLICE')
 print(frase[9:21:2])
